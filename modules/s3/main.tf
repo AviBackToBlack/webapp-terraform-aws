27820d7d5350ca43e714758ddeb8b5e88f703bb6
AviBackToBlack <avi@example.com>
2021-11-28 20:46:49 +0000

    Fixed: Allow destroying non-empty buckets

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,6 +1,7 @@
 resource "aws_s3_bucket" "s3_bucket" {
   bucket_prefix = "${var.project_name}-${var.user_role}-"
-  acl = "private"
+  acl           = "private"
+  force_destroy = true
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
